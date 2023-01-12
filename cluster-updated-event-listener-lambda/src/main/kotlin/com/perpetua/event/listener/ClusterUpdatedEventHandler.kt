cd6c703cd01559aa58f565b6193a116b8bac8c99
ivan-blagojevic <ivan.blagojevic@sellics.com>
2023-01-12 10:44:26 +0100

    Add sns publisher

--- cluster-updated-event-listener-lambda/src/main/kotlin/com/perpetua/event/listener/ClusterUpdatedEventHandler.kt
+++ cluster-updated-event-listener-lambda/src/main/kotlin/com/perpetua/event/listener/ClusterUpdatedEventHandler.kt
@@ -1,15 +1,35 @@
 package com.perpetua.event.listener
 
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain
+import com.amazonaws.regions.Regions
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler
+import com.amazonaws.services.sns.AmazonSNS
+import com.amazonaws.services.sns.AmazonSNSClientBuilder
+import com.amazonaws.services.sns.model.PublishRequest
 import java.io.InputStream
 import java.io.OutputStream
 
-class ClusterUpdatedEventHandler: RequestStreamHandler {
+class ClusterUpdatedEventHandler : RequestStreamHandler {
 
-    override fun handleRequest(p0: InputStream?, p1: OutputStream?, p2: Context?) {
-        TODO("Not yet implemented")
+    companion object {
+        val CLUSTER_UPDATED_TOPIC_ARN: String = System.getenv("CLUSTER_UPDATED_TOPIC")
     }
+
+    override fun handleRequest(inputStream: InputStream, outputStream: OutputStream, context: Context) {
+        val messageAsString = inputStream.bufferedReader().use { it.readText() }
+        getSnsClient().publish(getPublishRequest(messageAsString))
+    }
+
+    private fun getPublishRequest(message: String): PublishRequest {
+        val publishRequest = PublishRequest()
+        publishRequest.message = message
+        publishRequest.topicArn = CLUSTER_UPDATED_TOPIC_ARN
+        return publishRequest
+    }
+
+    private fun getSnsClient(): AmazonSNS = AmazonSNSClientBuilder.standard().withRegion(Regions.EU_WEST_1)
+        .withCredentials(DefaultAWSCredentialsProviderChain()).build()
 
 
 }
